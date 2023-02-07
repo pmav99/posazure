fb858f0fcc907cb7992a8ff4679d4afc99e140de
Panos Mavrogiorgos <pmav99@gmail.com>
2023-02-08 01:05:04 +0200

    update

--- start_task.sh
+++ start_task.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 #
 
-set -xeuo pipefail
+set -xEeuo pipefail
 
+whoami
 
+env | sort
+
+: "Install dependencies"
+sudo apt -yq update
+sudo apt install -yq \
+  silversearcher-ag \
+  nfs-common \
+;
+
+: "Mount Blob over nfs"
+sudo mkdir -p /blob
+# sudo mount -o sec=sys,vers=3,nolock,proto=tcp sabatchd75dposeidondev.blob.core.windows.net:/sabatchd75dposeidondev/test-nfs /blob
+sudo mount -t nfs -o vers=3,rsize=1048576,wsize=1048576,hard,nolock,proto=tcp,nconnect=8 sabatchd75dposeidondev.blob.core.windows.net:/sabatchd75dposeidondev/test-nfs /blob
+sudo chmod o+rx /blob
+sudo mkdir /blob/pool_output
+sudo chown batchadmin:batchadmin /blob/pool_output
