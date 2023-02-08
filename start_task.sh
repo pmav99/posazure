980cc8f3c62b27065e159779f647411a50d52993
Panos Mavrogiorgos <pmav99@gmail.com>
2023-02-08 12:52:39 +0200

    start_task: Add timestamps to the logs

--- start_task.sh
+++ start_task.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 #
 
-set -xEeuo pipefail
+set -Eeuo pipefail
 
-echo "START of START_TASK"
+log() {
+  printf '%s - %05d sec - %s\n' "$(date +'%T.%3N')" ${SECONDS} "${1}"
+  SECONDS=0
+}
 
-echo "Current user is: $(whoami)"
-echo "Current working directory is: $(pwd)"
-echo "The env variables are:"
+
+echo
+log "START of START_TASK"
+echo
+
+log "Current user is: $(whoami)"
+log "Current working directory is: $(pwd)"
+log "The env variables are:"
 echo
 env | sort
 echo
 getent passwd
 echo
-echo "Install dependencies"
+
+log "Test sudo access"
+sudo touch /dev/null
+
+
+log "Install dependencies"
 sudo apt -yq update
 sudo apt install -yq \
   silversearcher-ag \
@@ -21,7 +34,7 @@ sudo apt install -yq \
 ;
 
 echo
-echo "Setup conda"
+log "Setup conda"
 wget https://raw.githubusercontent.com/pmav99/posazure/master/condarc -O ~/.condarc
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p ~/miniconda
@@ -29,17 +42,19 @@ eval "$(~/miniconda/condabin/conda shell.bash hook)"
 source ~/miniconda/etc/profile.d/conda.sh
 which conda
 
-echo "Setup conda env"
-conda create --name schism_env "pschism=5.9*=mpi_mpich*"
+log "Setup conda env"
+conda create --quiet --name schism_env "pschism=5.9*=mpi_mpich*"
+
+log "Test conda env"
 conda activate schism_env
 which schism
 
-echo "Mount Blob over nfs"
+log "Mount Blob over nfs"
 sudo mkdir -p /blob
 # sudo mount -o sec=sys,vers=3,nolock,proto=tcp sabatchd75dposeidondev.blob.core.windows.net:/sabatchd75dposeidondev/test-nfs /blob
 sudo mount -t nfs -o vers=3,rsize=1048576,wsize=1048576,hard,nolock,proto=tcp,nconnect=8 sabatchd75dposeidondev.blob.core.windows.net:/sabatchd75dposeidondev/test-nfs /blob
 sudo chmod o+rx /blob
-sudo mkdir /blob/pool_output
+sudo mkdir -p /blob/pool_output
 sudo chown batchadmin:batchadmin /blob/pool_output
 
-echo "End of START_TASK"
+log "END of START_TASK"
