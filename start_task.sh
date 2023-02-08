92356302a704e40862c8d2bf4747798e9808d225
Panos Mavrogiorgos <pmav99@gmail.com>
2023-02-08 11:56:20 +0200

    start_task: Download condarc

--- start_task.sh
+++ start_task.sh
@@ -22,7 +22,8 @@ sudo apt install -yq \
 
 echo
 echo "Setup conda"
-wget -O ~/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget https://raw.githubusercontent.com/pmav99/posazure/master/condarc -O ~/.condarc
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p ~/miniconda
 eval "$(~/miniconda/condabin/conda shell.bash hook)"
 source ~/miniconda/etc/profile.d/conda.sh
